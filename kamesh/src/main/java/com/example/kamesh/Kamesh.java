c90af6abd683513ab1430eea23f6f6f3521bb45b
narendra <narendra@webdevelopmentpark.com>
2022-03-02 17:51:42 +0530

    updated

---
+++ kamesh/src/main/java/com/example/kamesh/Kamesh.java
@@ -0,0 +1,14 @@
+package com.example.kamesh;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Kamesh {
+
+    public static void show(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
