22bd19b119c384bb806c77dc97b9b118f92bfd94
narendra <narendra@webdevelopmentpark.com>
2022-03-03 14:17:59 +0530

    aa

--- kamesh/src/main/java/com/example/kamesh/Kamesh.java
+++ kamesh/src/main/java/com/example/kamesh/Kamesh.java
@@ -1,13 +1,15 @@
 package com.example.kamesh;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class Kamesh {
 
-    public static void show(Context c, String message){
+    public static void show(Context c){
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(c,Payment.class);
+        c.startActivity(intent);
 
     }
 
