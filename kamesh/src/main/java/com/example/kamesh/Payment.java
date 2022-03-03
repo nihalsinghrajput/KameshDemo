0e1933d39d177d44ee5278a97c0ac2ad53073362
narendra <narendra@webdevelopmentpark.com>
2022-03-03 12:02:57 +0530

    Updated new

--- kamesh/src/main/java/com/example/kamesh/Payment.java
+++ kamesh/src/main/java/com/example/kamesh/Payment.java
@@ -3,6 +3,7 @@ package com.example.kamesh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -28,9 +29,14 @@ public class Payment extends AppCompatActivity {
 
     }
 
-    public static void Play(Context context)
+    public void Play(Context context)
     {
 
-        Toast.makeText(context, "webviewStart", Toast.LENGTH_SHORT).show();
+        try {
+            Intent myIntent = new Intent(context,Class.forName("com.example.kameshisir.MainActivity"));
+            startActivity(myIntent );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
