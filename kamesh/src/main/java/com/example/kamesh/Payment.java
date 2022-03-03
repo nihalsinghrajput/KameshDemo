bcffb33040a82ee28e2e292b784044d31fe083ed
narendra <narendra@webdevelopmentpark.com>
2022-03-03 13:51:55 +0530

    hello

--- kamesh/src/main/java/com/example/kamesh/Payment.java
+++ kamesh/src/main/java/com/example/kamesh/Payment.java
@@ -2,6 +2,7 @@ package com.example.kamesh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.os.Handler;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class Payment extends AppCompatActivity {
@@ -32,11 +34,27 @@ public class Payment extends AppCompatActivity {
     public void Play(Context context)
     {
 
-        try {
-            Intent myIntent = new Intent(context,Class.forName("com.example.kameshisir.MainActivity"));
-            startActivity(myIntent );
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        final RelativeLayout layout = new RelativeLayout(context);
+
+        final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                getWindowManager().getDefaultDisplay().getWidth(),
+                getWindowManager().getDefaultDisplay().getHeight());
+
+        layout.setLayoutParams(params);
+
+        setContentView(layout);
+
+
+        final WebView web = new WebView(this);
+
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadData(
+                "<script type=\"text/javascript\" src=\"https://www.wdptechnologies.com/\"></script>",
+                null, null);
+
+        RelativeLayout.LayoutParams webViewParams = new RelativeLayout.LayoutParams(
+                300, 100);
+        webViewParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
     }
 }
